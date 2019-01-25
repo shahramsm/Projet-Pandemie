0627f1cc7525d107b1485bd363a6e37e677404e9
shahram <shahramsm@hotmail.com>
2019-01-25 16:53:59 +0100

    implementation methode moveto

---
+++ src/main/java/fr/dauphine/ja/student/pandemiage/gameengine/Player.java
@@ -0,0 +1,79 @@
+package fr.dauphine.ja.student.pandemiage.gameengine;
+
+import java.util.List;
+
+import fr.dauphine.ja.pandemiage.common.Disease;
+import fr.dauphine.ja.pandemiage.common.PlayerCardInterface;
+import fr.dauphine.ja.pandemiage.common.PlayerInterface;
+import fr.dauphine.ja.pandemiage.common.UnauthorizedActionException;
+
+public class Player implements PlayerInterface {
+	
+	private String location="";
+	private int cpt;
+	private GameEngine g;
+	
+	public Player(GameEngine g){
+		this.g = g;
+		this.cpt = 4;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Override
+	public void moveTo(String cityName) {
+		if(g.neighbours(location).contains(cityName)){
+			this.location = cityName;
+		}
+	}
+
+	@Override
+	public void flyTo(String cityName) throws UnauthorizedActionException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void flyToCharter(String cityName) throws UnauthorizedActionException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void skipTurn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void treatDisease(Disease d) throws UnauthorizedActionException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void discoverCure(List<PlayerCardInterface> cardNames) throws UnauthorizedActionException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String playerLocation() {
+		// TODO Auto-generated method stub
+		return this.location;
+	}
+
+	@Override
+	public List<PlayerCardInterface> playerHand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
