dfdc794ae4d7a0d1ec11012064494ae6bb39b523
touady18 <44091766+touady18@users.noreply.github.com>
2019-01-30 18:17:02 +0100

    new Update Gui

--- src/main/java/fr/dauphine/ja/student/pandemiage/ui/Gui.java
+++ src/main/java/fr/dauphine/ja/student/pandemiage/ui/Gui.java
@@ -17,12 +17,14 @@ import java.io.InputStream;
 import java.lang.*;
 import javafx.scene.Node;
 import javafx.scene.shape.Shape;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import fr.dauphine.ja.student.pandemiage.gameengine.City;
 import fr.dauphine.ja.student.pandemiage.gameengine.GameEngine;
 import javafx.application.Application; 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -72,28 +74,33 @@ public class Gui extends Application{
         ArrayList<Circle> circles = new ArrayList();
         // Transform cities to circles
         circles = createCircles(cities);
+        ArrayList<Text> texts = new ArrayList();
+        texts = createTexts(cities);
 
         Group root = new Group();
-        Scene s = new Scene(root, 1000,600);
+        Scene s = new Scene(root, 1300,650);
         
         
         // Background pandemic
-        final ImageView selectedImage = new ImageView();   
-        Image image = new Image(Gui.class.getResourceAsStream("image3.jpg"));
+        final ImageView imageView = new ImageView();   
+        Image image = new Image(Gui.class.getResourceAsStream("image.jpg"));
         
-        selectedImage.setImage(image);
-        selectedImage.setFitWidth(1000);
-        selectedImage.setFitHeight(600);
-        root.getChildren().addAll(selectedImage);
-        
-        
+        imageView.setImage(image);
+        imageView.setFitWidth(1300);
+        imageView.setFitHeight(650);
+        root.getChildren().addAll(imageView);
+       
       
         // Adding circles to the Group
-        for(int i =0; i< circles.size();i++) {
+       for(int i =0; i< circles.size();i++) {
     	   root.getChildren().add(circles.get(i));
 
         }
         
+        for(int i = 0; i< texts.size(); i++) {
+        	root.getChildren().add(texts.get(i));
+        }
+       
         stage.setScene(s);
         stage.show();
         
@@ -106,49 +113,61 @@ public class Gui extends Application{
 	 *  One city by one
 	 *  By getting the X and Y from GameEngine 
 	 *  
+	 *  @param ArrayList<City> cities
+	 *  @return ArrayList<Circle> circles
 	 */
-    
-
-    public static ArrayList<Circle> createCircles(ArrayList<City> cities) {
+	
+	
+	public static ArrayList<Circle> createCircles(ArrayList<City> cities) {
     	ArrayList<Circle> circles = new ArrayList<>();
-    	
+    	double x = 0.0;
+    	double y = 0.0;
+
     
     	for(City c : cities) {
-    		Circle circle = new Circle();
-        	circle.setRadius(10);
-    		String d = "RED";
-    		switch (d) {
-    		case "RED" : 
-    			circle.setFill(Color.RED);
-    			break;
-    		case "BLUE":
-    			circle.setFill(Color.BLUE);
-    			break;
-    		case "YELLOW": 
-    			circle.setFill(Color.YELLOW);
-    			break;
-    		case "BLACK" : 
-    			circle.setFill(Color.BLACK);
-    			break;
-    		}
-    		if(Double.parseDouble(c.getX())>800) {
-    			circle.setCenterX(Math.abs(Double.parseDouble(c.getX()))-200);
-        		circle.setCenterY(Math.abs(Double.parseDouble(c.getY())/2));
-        		circles.add(circle);
-        		System.out.println(circle);
+    			Circle circle = new Circle();
+    			circle.setRadius(10);
     			
-    		}
-    		else {
-    			circle.setCenterX(Math.abs(Double.parseDouble(c.getX())));
-        		circle.setCenterY(Math.abs(Double.parseDouble(c.getY())/2));
+    			circle.setCenterX((1600+Double.parseDouble(c.getX()))/2.5);
+        		circle.setCenterY((1500-Double.parseDouble(c.getY()))/2.5);
+        	
         		circles.add(circle);
         		System.out.println(circle);
-    		}
     	}
    
     	return circles;
     	  	
     }
+	
+	/*
+	 *  This method get the names of cities
+	 *  One city by one
+	 *  using getName() method
+	 *  
+	 *  @param ArrayList<City> cities
+	 *  @return ArrayList<Text> texts
+	 *  
+	 */	
+
+	public static ArrayList<Text> createTexts(ArrayList<City> cities){
+		ArrayList<Text> texts = new ArrayList<Text>();
+		for(City c : cities) {
+			Text t = new Text (c.getName());
+			
+				t.setX(((1600+Double.parseDouble(c.getX()))/2.5)+7);
+				t.setY(((1500-Double.parseDouble(c.getY()))/2.5)+7);
+				t.setFill(Color.WHITE);
+				texts.add(t);
+		}
+		
+		return texts;
+		
+	}
+	
+	
+	
+   //Start launch 
+
     public static void main(String[] args) {
        launch();
     	
